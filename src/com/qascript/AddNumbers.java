13f36f54812b9fc54d8e558905a7b38f451cbf72
asfandtariq90 <asfandtariq90@gmail.com>
2023-11-07 17:38:19 +0500

    Add numbers, For loop, while loop, Methods

--- src/com/qascript/AddNumbers.java
+++ src/com/qascript/AddNumbers.java
@@ -7,9 +7,15 @@ public class AddNumbers {
         addnumbers(10,20);
         addnumbers(100,200);
 
+        int result = addnumbers(4,5);
+        int output = result + 10;
+        System.out.println("Increment the output: " + output) ;
+
+
     }
-    public static void addnumbers(int num1, int num2){
-        int result = num1 + num2;
-        System.out.println("Addition of 2 numbers: " + result);
+    public static int addnumbers(int num1, int num2){
+        int res = num1 + num2;
+        System.out.println("Addition of 2 numbers: " + res);
+        return res;
     }
 }
