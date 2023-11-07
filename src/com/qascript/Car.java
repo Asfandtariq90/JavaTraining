ad7e1918a124680d9147256b75bc609131fc336a
asfandtariq90 <asfandtariq90@gmail.com>
2023-11-07 12:58:55 +0500

    Car Details

--- src/com/qascript/Car.java
+++ src/com/qascript/Car.java
@@ -1,2 +1,21 @@
-package com.qascript;public class Car {
+package com.qascript;
+
+public class Car {
+    public static void main(String[] args){
+
+        String carName = "Ferarri";
+        int carPrice = 100000;
+        double weight = 1000.50;
+        char logo = 'F';
+        boolean isAvailable = true;
+
+        System.out.println("Car Name: " + carName);
+        System.out.println("Car Price: " + carPrice);
+        System.out.println("Car Weight: " + weight);
+        System.out.println("Car Logo: " + logo);
+        System.out.println("Car Availability: " + isAvailable);
+
+
+    }
+
 }
